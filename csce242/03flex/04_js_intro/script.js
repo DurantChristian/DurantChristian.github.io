70a048c0db88abeb0b3e80192d7b33ed31258879
DurantChristian <cdd7@email.sc.edu>
2025-02-04 09:43:16 -0500

    pushed sript.js example

--- csce242/03flex/04_js_intro/script.js
+++ csce242/03flex/04_js_intro/script.js
@@ -3,3 +3,11 @@ document.getElementById("btn-click.me").onclick = () => {
     console.log("WOW!");
     document.getElementById("result").innerHTML="Hi Chris";
 };
+
+//change color
+document.getElementById("btn-color").onclick = () => {
+    console.log("Hello world");
+    const messageP = document.getElementById("message");
+    messageP.innerHTML = "Good bye";
+    messageP.classList.add("sad");
+}
