0c16fcc26a0459cd2961147174f1b3791a624c3d
DurantChristian <cdd7@email.sc.edu>
2025-02-06 09:27:08 -0500

    edited textbox

--- csce242/03flex/04_js_intro/script.js
+++ csce242/03flex/04_js_intro/script.js
@@ -11,3 +11,27 @@ document.getElementById("btn-color").onclick = () => {
     messageP.innerHTML = "Good bye";
     messageP.classList.toggle("sad");
 }
+
+//happy script
+document.getElementById("btn-happy").onclick = () => {
+    const displayP = document.getElementById("display");
+    displayP.classList.remove("hidden");
+    displayP.classList.add("happy");
+    displayP.innerHTML = "Good Times";
+};
+
+//sad sccript
+document.getElementById("btn-sad").onclick = () => {
+    const displayP = document.getElementById("display");
+    displayP.classList.remove("hidden");
+    displayP.classList.add("sad");
+    displayP.innerHTML = "Bad Times";
+}
+
+document.getElementById("btn-clear").onclick = () => {
+    document.getElementById("display").classList.add("hidden");
+};
+
+document.getElementById("textbox").onclick = () => {
+    const displayP = document.getElementById("display");
+}
