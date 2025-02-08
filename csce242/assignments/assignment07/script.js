124c07af52c5a3adeab07529a2c2a38f73f57fee
DurantChristian <cdd7@email.sc.edu>
2025-02-08 00:13:10 -0500

    edited assignment7

--- csce242/assignments/assignment07/script.js
+++ csce242/assignments/assignment07/script.js
@@ -1,17 +1,23 @@
-function addHello(card) {
-    let helloDiv = card.querySelector('.hello-text');
-    let newHello = document.createElement('p');
-    newHello.textContent = 'Hello';
-    helloDiv.appendChild(newHello);
-}
-
-document.getElementById("colorPicker").addEventListener("input", function () {
-    document.querySelector(".star").style.color = this.value;
+document.getElementById(".hello-card").forEach(card => {
+    card.onclick = () => {
+        console.log("Card Clicked!");
+        const helloDiv = card.getElementById(".hello-text");
+        const newHello = document.createElement("p");
+        newHello.innerHTML = "Hello";
+        helloDiv.appendChild(newHello);
+    };
 });
 
-function changeImage(imgElement) {
-    let image1 = "https://fakeimg.pl/200x200/A6E3A1/909090";
-    let image2 = "https://fakeimg.pl/200x200/C9CBFF/909090";
+document.getElementById("colorPicker").oninput = (event) => {
+    console.log("Color Selected:", event.target.value);
+    document.getElementById(".star").style.color = event.target.value;
+};
+
+document.getElementById(".toggle-image").onclick = (event) => {
+    console.log("Image Clicked!");
+    const imgElement = event.target;
+    const image1 = "https://fakeimg.pl/200x200/A6E3A1/909090";
+    const image2 = "https://fakeimg.pl/200x200/C9CBFF/909090";
 
     imgElement.src = imgElement.src.includes(image1) ? image2 : image1;
-}
+};
