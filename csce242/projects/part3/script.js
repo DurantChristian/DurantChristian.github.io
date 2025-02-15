fd474ff1bd690bcfdbb38b1d3b97197ae30e14f2
DurantChristian <cdd7@email.sc.edu>
2025-02-14 23:43:17 -0500

    added toggle to website

--- csce242/projects/part3/script.js
+++ csce242/projects/part3/script.js
@@ -1,6 +1,6 @@
 //toggling the nav
 const toggleMenu = () => {
     const menu = document.getElementById("nav-items");
-    menu.classList.toggle("show");
+    menu.classList.toggle("hidden");
 };
 document.getElementById("toggle-nav").onclick = toggleMenu;
