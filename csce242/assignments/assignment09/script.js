0bdd71d9b8a40a3c48cbf39e40edb030733b3c69
DurantChristian <cdd7@email.sc.edu>
2025-02-18 19:10:09 -0500

    edited assignment9

--- csce242/assignments/assignment09/script.js
+++ csce242/assignments/assignment09/script.js
@@ -1,48 +1,39 @@
-document.getElementById("drawLadder").addEventListener("click", () => {
+document.getElementById("drawStairs").addEventListener("click", () => {
     let container = document.getElementById("container");
     container.innerHTML = "";
-    let ladder = document.createElement("div");
-    ladder.className = "ladder";
-    container.appendChild(ladder);
+    let stair = document.createElement("div");
+    stair.className = "stair";
+    container.appendChild(stair);
     
     for (let i = 0; i < 10; i++) {
         let rung = document.createElement("div");
         rung.className = "rung";
         rung.style.bottom = (i * 25) + "px";
-        rung.style.position = "absolute";
         ladder.appendChild(rung);
     }
     
-    stickFigure.id = "stickFigure";
-            stickFigure.src = "/csce242/assignments/assignment09/images/left.png";
-            stickFigure.style.width = "50px";
-            stickFigure.style.position = "absolute";
-            stickFigure.style.left = "50%";
-            stickFigure.style.transform = "translateX(-50%)";
-            stickFigure.style.bottom = "12px";
-            stickFigure.style.display = "block";
-            container.appendChild(stickFigure);
-            
-            document.getElementById("controls").style.display = "block";
-            document.getElementById("author").style.display = "none";
-        });
-        
-        document.getElementById("climbLadder").addEventListener("click", () => {
-            let stickFigure = document.getElementById("stickFigure");
-            let position = 12;
-            let step = 25;
-            let maxPosition = step * 9;
-            let images = ["/csce242/assignments/assignment09/images/left.png", "/csce242/assignments/assignment09/images/right.png"];
-            let imgIndex = 0;
-            
-            let interval = setInterval(() => {
-                if (position >= maxPosition) {
-                    clearInterval(interval);
-                } else {
-                    position += step;
-                    stickFigure.style.bottom = position + "px";
-                    stickFigure.src = images[imgIndex % 2];
-                    imgIndex++;
-                }
-            }, 500);
-        });
+    let stickFigure = document.getElementById("stickFigure");
+    stickFigure.style.display = "block";
+    stickFigure.style.bottom = "0px";
+    document.getElementById("climbStairs").style.display = "block";
+});
+
+document.getElementById("climbStairs").addEventListener("click", () => {
+    let stickFigure = document.getElementById("stickFigure");
+    let position = 0;
+    let step = 25;
+    let maxPosition = step * 10;
+    let images = ["csce242/assignments/assignment09/images/left.png", "csce242/assignments/assignment09/images/right.png"];
+    let imgIndex = 0;
+    
+    let interval = setInterval(() => {
+        if (position >= maxPosition) {
+            clearInterval(interval);
+        } else {
+            position += step;
+            stickFigure.style.bottom = position + "px";
+            stickFigure.src = images[imgIndex % 2];
+            imgIndex++;
+        }
+    }, 500);
+});
