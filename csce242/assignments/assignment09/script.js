00f67e3a6bf6ba93423ffbf2b8e51dbd30d34ac5
DurantChristian <cdd7@email.sc.edu>
2025-02-19 11:46:30 -0500

    edited stickfigure position

--- csce242/assignments/assignment09/script.js
+++ csce242/assignments/assignment09/script.js
@@ -22,7 +22,7 @@ document.getElementById("climbStairs").addEventListener("click", () => {
     let stickFigure = document.getElementById("stickFigure");
     let position = 262;
     let step = 15;
-    let maxPosition = step * 10;
+    let maxPosition = step * 8;
     let images = ["/csce242/assignments/assignment09/images/left.png", "/csce242/assignments/assignment09/images/right.png"];
     let imgIndex = 0;
     
