11431dfc1bd3dd2daadcf7e3d070f4d931d271f4
DurantChristian <cdd7@email.sc.edu>
2025-02-26 20:43:59 -0500

    edited assingment 11

--- csce242/assignments/assignment11/script.js
+++ csce242/assignments/assignment11/script.js
@@ -1,15 +1,17 @@
 class Pizza {
-    constructor(name, image, ingredients, price, size, crustType) {
+    constructor(name, image, ingredients, sauce, cheese, price, size, crustType) {
         this.name = name;
         this.image = image;
         this.ingredients = ingredients;
+        this.sauce = sauce;
+        this.cheese = cheese;
         this.price = price;
         this.size = size;
         this.crustType = crustType;
     }
 
     getSection = () => `
-        <div class="pizza" onclick="showModal('${this.name}', '${this.image}', '${this.ingredients}', '${this.price}')">
+        <div class="pizza" onclick="showModal('${this.name}', '${this.image}', '${this.ingredients}', '${this.sauce}', '${this.cheese}', '${this.price}')">
             <h2>${this.name}</h2>
             <img src="${this.image}" alt="${this.name}">
         </div>
@@ -17,11 +19,11 @@ class Pizza {
 }
 
 const pizzas = [
-    new Pizza("Hawaiian", "/csce242/assignments/assignment11/images/hawaiian.png", "Ham, Pineapple, Cheese", 12.99, "Large", "Thin Crust"),
-    new Pizza("Buffalo Chicken Ranch", "/csce242/assignments/assignment11/images/buffalo.png", "Chicken, Ranch, Cheese", 14.99, "Medium", "Stuffed Crust"),
-    new Pizza("Margarita", "/csce242/assignments/assignment11/images/Margarita.png", "Tomatoes, Basil, Mozzarella", 10.99, "Small", "Thin Crust"),
-    new Pizza("Pepperoni", "/csce242/assignments/assignment11/images/pepperoni.png", "Pepperoni, Cheese, Tomato Sauce", 11.99, "Large", "Regular Crust"),
-    new Pizza("Veggie", "/csce242/assignments/assignment11/images/veggie.png", "Mushrooms, Peppers, Olives", 13.99, "Medium", "Thin Crust")
+    new Pizza("Hawaiian", "/csce242/assignments/assignment11/images/hawaiian.png", "Ham, Pineapple", "Tomato Sauce", "Mozzarella", 12.99, "Medium", "Thin Crust"),
+    new Pizza("Buffalo Chicken Ranch", "/csce242/assignments/assignment11/images/buffalo.png", "Chicken, Ranch", "Ranch Sauce", "Cheddar", 14.99, "Medium", "Stuffed Crust"),
+    new Pizza("Margarita", "/csce242/assignments/assignment11/images/Margarita.png", "Tomatoes, Basil", "Tomato Sauce", "Fresh Mozzarella", 10.99, "Small", "Thin Crust"),
+    new Pizza("Pepperoni", "/csce242/assignments/assignment11/images/pepperoni.png", "Pepperoni", "Tomato Sauce", "Mozzarella", 11.99, "Large", "Regular Crust"),
+    new Pizza("Veggie", "/csce242/assignments/assignment11/images/veggie.png", "Mushrooms, Peppers, Olives", "Garlic Sauce", "Vegan Cheese", 13.99, "Medium", "Thin Crust")
 ];
 
 const pizzaContainer = document.getElementById("pizzaContainer");
@@ -30,10 +32,12 @@ pizzas.forEach(pizza => {
     pizzaContainer.innerHTML += pizza.getSection();
 });
 
-const showModal = (name, image, ingredients, price) => {
+const showModal = (name, image, ingredients, sauce, cheese, price) => {
     document.getElementById("modalTitle").innerText = name;
     document.getElementById("modalImage").src = image;
     document.getElementById("modalIngredients").innerText = ingredients;
+    document.getElementById("modalSauce").innerText = sauce;
+    document.getElementById("modalCheese").innerText = cheese;
     document.getElementById("modalPrice").innerText = price;
     
     const modal = document.getElementById("pizzaModal");
