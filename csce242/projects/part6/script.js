cc603e0bc99a06f45e62974e6267d9332bef0fa5
DurantChristian <cdd7@email.sc.edu>
2025-02-28 17:12:12 -0500

    edited project1 part6

--- csce242/projects/part6/script.js
+++ csce242/projects/part6/script.js
@@ -11,33 +11,52 @@ const getReviews = async () => {
 
 const showReviews = async () => {
     let gameReviews = await getReviews();
-    let reviewscontainer = document.getElementById("reviews-container")
+    let reviewsContainer = document.getElementById("reviews-container");
+
+    if (!reviewsContainer) {
+        console.error("Element with ID 'reviews-container' not found.");
+        return;
+    }
+
+    reviewsContainer.innerHTML = "";
 
     gameReviews.forEach((gamereview) => {
-        reviewsSection.append(getReviewItem(gamereview));
+        reviewsContainer.append(getReviewItem(gamereview));
     });
 };
 
 const getReviewItem = (gamereview) => {
     let section = document.createElement("section");
+    section.classList.add("review");
+
     let h3 = document.createElement("h3");
     h3.innerText = gamereview.title;
     section.append(h3);
+
+    let img = document.createElement("img");
+    img.src = `images/${gamereview.img_name}`; 
+    img.alt = gamereview.title;
+    section.append(img);
+
     let ul = document.createElement("ul");
+    ul.append(getLi(`Genre: ${gamereview.genre}`));
+    ul.append(getLi(`Developer: ${gamereview.developer}`));
+    ul.append(getLi(`Release Year: ${gamereview.release_year}`));
+    ul.append(getLi(`Rating: ${gamereview.rating}`));
     section.append(ul);
-    ul.append(getLi(gamereview.img_name));
-    ul.append(getLi(gamereview.features));
-    ul.append(getLi(gamereview.genre));
-    ul.append(getLi('Rating: ${gamereview.rating}'));
-    ul.append(getLi(gamereview.developer));
-    ul.append(getLi(gamereview.release_year));
-    ul.append(getLi(gamereview.external_link));
+
+    let link = document.createElement("a");
+    link.href = gamereview.external_link;
+    link.innerText = "Read More";
+    link.target = "_blank";
+    section.append(link);
 
     section.append(getReview(gamereview.reviews));
 
     return section;
 };
-const getLi = data => {
+
+const getLi = (data) => {
     const li = document.createElement("li");
     li.textContent = data;
     return li;
@@ -45,7 +64,7 @@ const getLi = data => {
 
 const getReview = (reviews) => {
     const section = document.createElement("section");
-    reviews.forEach(review =>{
+    reviews.forEach((review) => {
         const p = document.createElement("p");
         p.textContent = review;
         section.append(p);
