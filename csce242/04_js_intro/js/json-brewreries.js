3c49e555aa046ca766c537df7ddbb61195da9986
DurantChristian <cdd7@email.sc.edu>
2025-03-04 09:40:04 -0500

    json parson ex

--- csce242/04_js_intro/js/json-brewreries.js
+++ csce242/04_js_intro/js/json-brewreries.js
@@ -11,3 +11,40 @@ const getMap = async(latitude, longitude) => {
         console.log(error);
     }
 };
+
+const getBreweries = async() => {
+    const url = "https://api.openbrewerydb.org/breweries";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch(error){
+        console.log(error);
+    }
+};
+
+const showBreweries = async() => {
+    const breweriesSection = document.getElementById("breweries-section");
+    const breweries = await getBreweries();
+    
+    breweries.forEach((brewery)=> {
+        const section = document.createElement("section");
+        section.classList.add("brewery");
+        breweriesSection.append(section);
+
+        const a = document.createElement("a");
+        a.href = brewery.website_url;
+        section.append(a);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = brewery.name;
+        section.append(h3);
+        
+        const p = document.createElement("p");
+        p.innerHTML = brewery.brewery_type + " brewery";
+        section.append(p);
+    });
+    
+};
+
+showBreweries();
