d17109d76c9cb2d914a7fd44acec77511179418a
DurantChristian <cdd7@email.sc.edu>
2025-03-04 15:15:37 -0500

    edited project part6

--- csce242/projects/part6/script.js
+++ csce242/projects/part6/script.js
@@ -8,63 +8,46 @@ const getReviews = async () => {
         console.log(error);
     }
 };
+
 const showReviews = async () => {
-    let gameReviews = await getReviews();
+    const reviewsSection = document.getElementById("reviews-container");
+    const reviews = await getReviews();
 
-    let reviewsContainer = document.getElementById("reviews-container");
+reviews.forEach(async(review) => {
+    const section = document.createElement("section");
+    section.classList.add("review");
+    reviewsSection.append(section);
 
-    reviewsContainer.innerHTML = "";
-
-    gameReviews.forEach(async(gamereview) => {
-        reviewsContainer.append(getReviewItem(gamereview));
-    });
-};
-
-const getReviewItem = (gamereview) => {
-    let section = document.createElement("section");
-    section.classList.add("reviews-container");
-
-    let h3 = document.createElement("h3");
-    h3.innerText = gamereview.title;
+    const h3 = document.createElement("h3");
+    h3.innerText = review.title;
     section.append(h3);
 
-    let img = document.createElement("img");
+    const img = document.createElement("img");
     img.src = `csce242/projects/part6/images/${gamereview.img_name}`; 
-    img.alt = gamereview.title;
+    img.alt = review.title;
     section.append(img);
 
-    let ul = document.createElement("ul");
-    ul.append(getLi(`Genre: ${gamereview.genre}`));
-    ul.append(getLi(`Developer: ${gamereview.developer}`));
-    ul.append(getLi(`Release Year: ${gamereview.release_year}`));
-    ul.append(getLi(`Rating: ${gamereview.rating}`));
+    const ul = document.createElement("ul");
+    const details = [
+    `Genre: ${review.genre}`,
+    `Developer: ${review.developer}`,
+    `Release Year: ${review.release_year}`,
+    `Rating: ${review.rating}`,
+];
+
+    details.forEach((detail) => {
+        const li = document.createElement("li");
+        li.textContent = detail;
+        ul.append(li);
+});
+
     section.append(ul);
 
-    let link = document.createElement("a");
-    link.href = gamereview.external_link;
+    const link = document.createElement("a");
+    link.href = review.external_link;
     link.innerText = "Read More";
     link.target = "_blank";
     section.append(link);
-
-    section.append(getReview(gamereview.reviews));
-
-    return section;
+});
 };
-
-const getLi = (data) => {
-    const li = document.createElement("li");
-    li.textContent = data.games;
-    return li;
-};
-
-const getReview = (reviews) => {
-    const section = document.createElement("section");
-    reviews.forEach((review) => {
-        const p = document.createElement("p");
-        p.textContent = review;
-        section.append(p);
-    });
-
-    return section;
-};
-window.onload = () => showReviews();
+showReviews();
