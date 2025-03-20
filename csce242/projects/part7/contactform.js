254b153e5a287c81730ab32f1e5b8db7e7032cb8
DurantChristian <cdd7@email.sc.edu>
2025-03-20 19:06:53 -0400

    edited contact form for trobuleshoot

--- csce242/projects/part7/contactform.js
+++ csce242/projects/part7/contactform.js
@@ -6,11 +6,12 @@ form.addEventListener('submit', function (e) {
 
     const formData = new FormData(form);
     const object = Object.fromEntries(formData);
-    object.access_key = "17cffc5-dfd0-4c25-8906-e6530a0c8308";
+    object.access_key = "817cffc5-dfd0-4c25-8906-e6530a0c8308";
     const json = JSON.stringify(object);
 
     result.style.display = "block";
     result.innerHTML = "Please wait...";
+    result.className = "message";
 
     fetch('https://api.web3forms.com/submit', {
         method: 'POST',
@@ -24,21 +25,21 @@ form.addEventListener('submit', function (e) {
         let json = await response.json();
         if (response.ok) {
             result.innerHTML = json.message;
-            result.className = "Message successfully sent";
+            result.classList.add("Message successfully sent.");
         } else {
             result.innerHTML = json.message;
-            result.className = "Sorry, your message was not sent";
+            result.classList.add("Sorry, your message was not sent.");
         }
     })
     .catch(error => {
         console.error(error);
         result.innerHTML = "Something went wrong. Please try again!";
-        result.className = "message error";
+        result.classList.add("error");
     })
     .then(() => {
         form.reset();
         setTimeout(() => {
             result.style.display = "none";
-        }, 3000);
+        }, 5000);
     });
 });
