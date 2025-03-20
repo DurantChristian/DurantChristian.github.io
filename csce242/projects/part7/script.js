33fb7116cada89bc2e70a70657a5466cf3afe610
DurantChristian <cdd7@email.sc.edu>
2025-03-20 14:48:58 -0400

    edited contact form for trobuleshoot

--- csce242/projects/part7/script.js
+++ csce242/projects/part7/script.js
@@ -7,9 +7,14 @@ const getReviews = async () => {
 
     try {
         const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
         return await response.json();
-    } catch(error) {
-        console.log(error);
+    } catch (error) {
+        console.error("Error fetching reviews:", error.message);
     }
 };
 
