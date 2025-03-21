a0d46f46077e3fe10683daee82d6c46dec9ffb62
DurantChristian <cdd7@email.sc.edu>
2025-03-21 12:17:02 -0400

    edited contact form for trobuleshoot

--- csce242/projects/part7/contactform.js
+++ csce242/projects/part7/contactform.js
@@ -1,45 +1,49 @@
-const form = document.getElementById('contactForm');
-const result = document.getElementById('responseMessage');
+const showEmailResult = async (e) => {
+    e.preventDefault(); 
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
+    const result = document.getElementById("result");
+    result.innerHTML = "Please wait...."; 
 
+    let response = await getEmailResult();
+
+    if (response && response.status === 200) {
+        result.innerHTML = "Email successfully sent!";
+        result.classList.add("success");
+    } else {
+        result.innerHTML = "Sorry, your email was not sent.";
+        result.classList.add("error");
+    }
+
+    setTimeout(() => {
+        result.innerHTML = "";
+    }, 5000);
+};
+
+const getEmailResult = async () => {
+    const form = document.getElementById("contact-form");
     const formData = new FormData(form);
     const object = Object.fromEntries(formData);
+    
     object.access_key = "817cffc5-dfd0-4c25-8906-e6530a0c8308";
+
     const json = JSON.stringify(object);
+    const result = document.getElementById("result");
 
-    result.style.display = "block";
-    result.innerHTML = "Please wait...";
-    result.className = "message";
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        });
 
-    fetch('https://api.web3forms.com/submit', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: json
-    })
-    .then(async (response) => {
-        let json = await response.json();
-        if (response.ok) {
-            result.innerHTML = json.message;
-            result.classList.add("Message successfully sent.");
-        } else {
-            result.innerHTML = json.message;
-            result.classList.add("Sorry, your message was not sent.");
-        }
-    })
-    .catch(error => {
+        return response;
+    } catch (error) {
         console.error(error);
-        result.innerHTML = "Something went wrong. Please try again!";
-        result.classList.add("error");
-    })
-    .then(() => {
-        form.reset();
-        setTimeout(() => {
-            result.style.display = "none";
-        }, 5000);
-    });
-});
+        result.innerHTML = "Sorry, your email couldn't be sent.";
+        return null;
+    }
+};
+document.getElementById("contact-form").onsubmit = showEmailResult;
